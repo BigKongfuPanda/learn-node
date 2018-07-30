764fd07bbd98d9e895c3ea1f6ce4f55785b96bfd
张海泉 <haiquan.zhang@tcredit.com>
2018-07-30 16:48:38 +0800

    http

--- express/myapp/public/javascripts/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('#btn').click(function() {
-        $.post('/users?name=z中文&id=121212', {age: 12, location: 'bj北京'}, (data) => {
-            console.log(data);
-        });
-    });
-});
