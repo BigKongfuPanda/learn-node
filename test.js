63b99de491a7bff135dc9f7a8ca354ee008b08b6
zhanghaiquan <zhqustb@163.com>
2018-07-31 21:41:42 +0800

    'pull'

--- test.js
+++ test.js
@@ -1,20 +1,39 @@
-const buf1 = Buffer.allocUnsafe(26);
-for (let i = 0; i < 26; i++) {
-    buf1[i] = i + 97;
-}
-const buf2 = buf1.slice(0, 3);
 
-console.log(buf2.toString('ascii', 0, buf2.length));
-// 输出：abc
+var http = require('http');
+var querystring = require('querystring');
+ 
+var postHTML = 
+  '<html><head><meta charset="utf-8"><title>菜鸟教程 Node.js 实例</title></head>' +
+  '<body>' +
+  '<form method="post">' +
+  '网站名： <input name="name"><br>' +
+  '网站 URL： <input name="url"><br>' +
+  '<input type="submit">' +
+  '</form>' +
+  '</body></html>';
+ 
+http.createServer(function (req, res) {
+  var body = "";
+  req.on('data', function (chunk) {
+    body += chunk;
+  });
+  req.on('end', function () {
+    // 解析参数
+    body = querystring.parse(body);
+    // 设置响应头部信息及编码
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
+    console.log(body);
+ 
+    if(body.name && body.url) { // 输出提交的数据
+        res.write("网站名：" + body.name);
+        res.write("<br>");
+        res.write("网站 URL：" + body.url);
+    } else {  // 输出表单
+        res.write(postHTML);
+    }
+    res.end();
+  });
+}).listen(3000);
 
-// 修改buf1, buf2也会跟着改变
-buf1[0] = 33;
-// 输出: !bc
-console.log(buf2.toString('ascii', 0, buf2.length));
 
-buf2[0] = 44;
-// 输出: ,bcdefghijklmnopqrstuvwxyz
-console.log(buf1.toString('ascii', 0, buf1.length));
 
-const buf3 = Buffer.alloc(3);
-console.log(buf3.toJSON());
