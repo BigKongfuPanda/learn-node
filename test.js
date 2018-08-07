b9180945b17bfaf25baf2921f8878bbab83604bd
zhanghaiquan <zhqustb@163.com>
2018-08-07 22:36:21 +0800

    'zlib'

--- test.js
+++ test.js
@@ -1,19 +1,47 @@
+const zlib = require('zlib');
+const fs = require('fs');
 const http = require('http');
-const url = require('url');
 
-// https.get('https://www.baidu.com', res => {
-//     console.log('status code: ', res.statusCode);
-//     console.log('headers: ', res.headers);
+// 客户端示例
+const request = http.get({
+    host: 'example.com',
+    path: '/',
+    port: 80,
+    headers: {'Accept-Encoding': 'gzip,deflate'}
+});
 
-//     res.on('data', data => {
-//         process.stdout.write(data);
-//     });
-// }).on('error', err => {
-//     console.log(err);
-// });
+request.on('response', response => {
+    const output = fs.createWriteStream('example.com_index.html');
+    switch (response.headers['content-encoding']) {
+        case 'gzip':
+            response.pipe(zlib.createGunzip()).pipe(output);
+            break;
+        case 'deflate':
+            response.pipe(zlib.createInflate()).pipe(output);
+            break;
+        default:
+            response.pipe(output);
+            break;
+    }
+});
 
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-    let queryObj = url.parse(req.url, true).query;
-    res.end(JSON.stringify(queryObj));
+
+//服务端的示例。压缩资源给response，必须得判断request的请求头中能够接受的encoding类型
+http.createServer((request, response) => {
+    const raw = fs.createReadStream('index.html');
+    let acceptEncoding = request.headers['accept-encoding'];
+    if(!acceptEncoding){
+        acceptEncoding = '';
+    }
+    // 判断acceptEncoding的类型
+    if (/\bdeflate\b/.test(acceptEncoding)) {
+        response.writeHead(200, {'Content-Encoding': 'deflate'});
+        raw.pipe(zlib.createDeflate()).pipe(response);
+    } else if (/\bgzip\b/.test(acceptEncoding)) {
+        response.writeHead(200, {'Content-Encoding': 'gzip'});
+        raw.pipe(zlib.createGzip()).pipe(response);
+    } else {
+        response.writeHead(200, {});
+        raw.pipe(response);
+    }
 }).listen(3000);
